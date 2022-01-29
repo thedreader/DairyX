914bc074809defc66447a185e240b4728b677496
Chirantan Paul <paulrivu@gmail.com>
2022-01-29 19:29:40 +0530

    edited app.js

--- app.js
+++ app.js
@@ -567,6 +567,11 @@ app.get('/logout', function (req, res) {
 
 
 
-app.listen(3000, function () {
-   console.log("listening on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server started on port 3000");
 });
